9fb42a967eee3c81e673db1353e26a86eef96664
Vinodh Kumar Mandhala <vinodhkumar.mandhala@ispatialtec.com>
2026-04-03 23:35:49 +0530

    fix cors for vercel frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,8 @@ app = FastAPI(
 # Set up CORS for Vite frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    # allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
